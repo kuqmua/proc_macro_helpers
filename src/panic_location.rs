f0ee76582d3e3f48b670a4d59b441bbe20914133
kuqmua <serezhashmakov@outlook.com>
2023-05-17 00:26:01 +0300

    panic_location()

--- src/panic_location.rs
+++ src/panic_location.rs
@@ -1,9 +1,9 @@
-pub fn panic_location(proc_macro_name: &'static str) {
+pub fn panic_location() {
     std::panic::set_hook(Box::new(move |panic_info| {
         if let Some(location) = panic_info.location() {
-            eprintln!("{proc_macro_name} panic occurred in {}:{}:{}", location.file(), location.line(), location.column());
+            eprintln!("panic occurred in {}:{}:{}", location.file(), location.line(), location.column());
         } else {
-            eprintln!("{proc_macro_name} panic occurred but can't get location information...");
+            eprintln!("panic occurred but can't get location information...");
         }
     }));
 }
