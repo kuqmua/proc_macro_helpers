f1f1e79020ea22e347759390d9d421259f93edb6
kuqmua <serezhashmakov@outlook.com>
2023-07-02 05:13:19 +0300

    pub static AST_PARSE_FAILED: &str = "let ast: syn::DeriveInput = syn::parse(input) failed";

--- src/global_variables/hardcode.rs
+++ src/global_variables/hardcode.rs
@@ -1,2 +1,3 @@
 pub static PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE: &str =
     ".parse::<proc_macro2::TokenStream>() failed";
+pub static AST_PARSE_FAILED: &str = "let ast: syn::DeriveInput = syn::parse(input) failed";
