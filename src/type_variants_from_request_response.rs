6a6fb6a977eec5d6c417cf3059fc26115de53aab
kuqmua <serezhashmakov@outlook.com>
2023-11-22 22:22:29 +0300

    next steps

--- src/type_variants_from_request_response.rs
+++ src/type_variants_from_request_response.rs
@@ -14,10 +14,16 @@ pub fn generate_tvfrr_extraction_logic_lower_case_token_stream(
     ident_lower_case_stringified: &str,
     proc_macro_name_ident_stringified: &str,
 ) -> proc_macro2::TokenStream {
-    let tvfrr_extraction_logic_stringified = format!("tvfrr_extraction_logic_{ident_lower_case_stringified}");
+    let tvfrr_extraction_logic_stringified =
+        format!("tvfrr_extraction_logic_{ident_lower_case_stringified}");
     tvfrr_extraction_logic_stringified
-    .parse::<proc_macro2::TokenStream>()
-    .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {tvfrr_extraction_logic_stringified} {}", crate::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
+        .parse::<proc_macro2::TokenStream>()
+        .unwrap_or_else(|_| {
+            panic!(
+                "{proc_macro_name_ident_stringified} {tvfrr_extraction_logic_stringified} {}",
+                crate::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE
+            )
+        })
 }
 
 pub fn generate_ident_request_error_camel_case_token_stream(
@@ -26,16 +32,26 @@ pub fn generate_ident_request_error_camel_case_token_stream(
 ) -> proc_macro2::TokenStream {
     let ident_request_error_stringified = format!("{ident}RequestError");
     ident_request_error_stringified
-    .parse::<proc_macro2::TokenStream>()
-    .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {ident_request_error_stringified} {}", crate::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
+        .parse::<proc_macro2::TokenStream>()
+        .unwrap_or_else(|_| {
+            panic!(
+                "{proc_macro_name_ident_stringified} {ident_request_error_stringified} {}",
+                crate::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE
+            )
+        })
 }
 
-pub fn generate_try_from_response_lower_case_token_stream(
+pub fn generate_try_from_response_ident_lower_case_token_stream(
     ident_lower_case_stringified: &str,
     proc_macro_name_ident_stringified: &str,
 ) -> proc_macro2::TokenStream {
     let try_from_response_stringified = format!("try_from_response_{ident_lower_case_stringified}");
     try_from_response_stringified
-    .parse::<proc_macro2::TokenStream>()
-    .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {try_from_response_stringified} {}", crate::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
+        .parse::<proc_macro2::TokenStream>()
+        .unwrap_or_else(|_| {
+            panic!(
+                "{proc_macro_name_ident_stringified} {try_from_response_stringified} {}",
+                crate::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE
+            )
+        })
 }
