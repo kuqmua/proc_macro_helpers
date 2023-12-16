9484a44a9a711c1eedbe41f0963fd12147593493
kuqmua <serezhashmakov@outlook.com>
2023-12-16 13:53:36 +0300

    next steps

--- src/get_macro_attribute.rs
+++ src/get_macro_attribute.rs
@@ -1,10 +1,10 @@
 pub fn get_macro_attribute<'a>(
     attrs: &'a [syn::Attribute],
-    attribute_path: std::string::String,
+    attribute_path: &std::string::String,
     proc_macro_name_ident_stringified: &std::string::String
 ) -> &'a syn::Attribute {
     let option_attribute = attrs.iter().find(|attr| {
-        attribute_path == {
+        *attribute_path == {
             let mut stringified_path = quote::ToTokens::to_token_stream(&attr.path).to_string();
             stringified_path.retain(|c| !c.is_whitespace());
             stringified_path
