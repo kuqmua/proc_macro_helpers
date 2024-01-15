a3072f97fec23235a7674b06c0200b0baf5c5417
kuqmua <serezhashmakov@outlook.com>
2024-01-15 18:48:28 +0300

    next steps

--- src/naming_conventions.rs
+++ src/naming_conventions.rs
@@ -1,10 +1,10 @@
-pub trait ToUpperCamelCase<T> {
+pub trait ToUpperCamelCase<T> {//todo rename all variable names like _upper_camel_case instead of _camel_case
     fn to_upper_camel_case(&self) -> std::string::String;
 }
 
 impl<T> ToUpperCamelCase<T> for T
 where
-    String: PartialEq<T>,
+    std::string::String: PartialEq<T>,
     Self: AsRef<str>,
 {
     fn to_upper_camel_case(&self) -> std::string::String {
@@ -18,11 +18,24 @@ pub trait ToLowerSnakeCase<T> {//todo rename as just snake case and all variable
 
 impl<T> ToLowerSnakeCase<T> for T
 where
-    String: PartialEq<T>,
+    std::string::String: PartialEq<T>,
     Self: AsRef<str>,
 {
     fn to_lower_snake_case(&self) -> std::string::String {
         convert_case::Casing::to_case(self, convert_case::Case::Snake)
     }
 }
-// todo SCREAMING_SNAKE_CASE
+
+pub trait ToScreamingSnakeCase<T> {
+    fn to_screaming_snake_case(&self) -> std::string::String;
+}
+
+impl<T> ToScreamingSnakeCase<T> for T
+where
+    std::string::String: PartialEq<T>,
+    Self: AsRef<str>,
+{
+    fn to_screaming_snake_case(&self) -> std::string::String {
+        convert_case::Casing::to_case(self, convert_case::Case::ScreamingSnake)
+    }
+}
