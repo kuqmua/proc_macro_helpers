fd01fd57ec70334226982d2e4ce998d03f169834
kuqmua <serezhashmakov@outlook.com>
2024-01-15 11:33:16 +0300

    next steps

--- src/naming_conventions.rs
+++ src/naming_conventions.rs
@@ -7,12 +7,12 @@ where
     String: PartialEq<T>,
     Self: AsRef<str>,
 {
-    fn to_upper_camel_case(&self) -> String {
+    fn to_upper_camel_case(&self) -> std::string::String {
         convert_case::Casing::to_case(self, convert_case::Case::UpperCamel)
     }
 }
 
-pub trait ToLowerSnakeCase<T> {
+pub trait ToLowerSnakeCase<T> {//todo rename as just snake case and all variable names
     fn to_lower_snake_case(&self) -> std::string::String;
 }
 
@@ -21,7 +21,7 @@ where
     String: PartialEq<T>,
     Self: AsRef<str>,
 {
-    fn to_lower_snake_case(&self) -> String {
-        convert_case::Casing::to_case(self, convert_case::Case::Snake).to_lowercase()
+    fn to_lower_snake_case(&self) -> std::string::String {
+        convert_case::Casing::to_case(self, convert_case::Case::Snake)
     }
 }
