ca42b002dbee4d3767b442677fe3033f1d0a05b1
kuqmua <serezhashmakov@outlook.com>
2024-01-22 09:45:19 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,6 @@ pub mod write_token_stream_into_file;
 pub mod get_macro_attribute;
 pub mod naming_conventions;
 
-/////
-
-
 // impl OperationHttpMethod {
 //     fn to_snake_case_token_stream(&self) -> proc_macro2::TokenStream {
 //         let value_snake_case_stringified = proc_macro_helpers::naming_conventions::ToSnakeCase::to_snake_case(self);
@@ -38,5 +35,22 @@ pub mod naming_conventions;
 //         );
 //         value.parse::<proc_macro2::TokenStream>()
 //         .unwrap_or_else(|_| panic!("{value} {}", proc_macro_common::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
+//     }
+// }
+
+#[derive(
+    proc_macro_assistants::ToSnakeCaseStringified,
+)]
+pub enum TestOperationPrintlnInfo {
+    Start,
+    End
+}
+
+// impl std::fmt::Display for TestOperationPrintlnInfo {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         match self {
+//             Self::Start => write!(f, "start"),
+//             Self::End => write!(f, "end")
+//         }
 //     }
 // }
